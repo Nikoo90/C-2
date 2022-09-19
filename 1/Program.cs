f6fafd48766634020438e94f168bf638cdd73c72
Nikoo90 <66570980+Nikoo90@users.noreply.github.com>
2022-09-19 22:18:52 +0300

    find second number

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = (num % 100 - num % 10) / 10;
+Console.WriteLine(result);
