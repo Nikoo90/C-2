3524a31b15c1aa81841ef6966094ea5d74cff357
Nikoo90 <66570980+Nikoo90@users.noreply.github.com>
2022-09-19 22:54:16 +0300

    Trird number

---
+++ 2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+if (num < 100) Console.WriteLine("no third digit");
+else
+{
+    if (num >= 1000)
+    {
+        while (true)
+        {
+            num /= 10;
+            if (num < 1000)
+            {
+                result = num % 10;
+                break;
+            }
+        }
+    }
+    else result = num % 10;
+
+    Console.WriteLine($"Trird number: {result}");
+}
